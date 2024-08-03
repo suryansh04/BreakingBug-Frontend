1019ff5bfa08b5e3c6006b56aaefc6757b6c822d
Sushrut <sushrut.kane4@gmail.com>
2024-08-03 17:09:43 +0530

    fixed the speedDialTemplete

--- src/components/SpeedDialTemplate.jsx
+++ src/components/SpeedDialTemplate.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
-import { SpeedDial, styled } from '@mui/material';
+import React from 'react';
+import { SpeedDial, SpeedDialAction, styled } from '@mui/material';
 import TuneIcon from '@mui/icons-material/Tune';
 
 const SpeedDialTemplate = ({ actions }) => {
+    // Define the taskHandler function
+    const taskHandler = (action) => {
+        // Implement what happens when an action is clicked
+        console.log(`Action ${action.name} clicked`);
+    };
+
     return (
         <CustomSpeedDial
             ariaLabel="SpeedDial playground example"
@@ -15,14 +21,14 @@ const SpeedDialTemplate = ({ actions }) => {
                     key={action.name}
                     icon={action.icon}
                     tooltipTitle={action.name}
-                    onClick={action.action}
+                    onClick={() => taskHandler(action)} // Use the taskHandler here
                 />
             ))}
         </CustomSpeedDial>
-    )
+    );
 }
 
-export default SpeedDialTemplate
+export default SpeedDialTemplate;
 
 const CustomSpeedDial = styled(SpeedDial)`
   .MuiSpeedDial-fab {
